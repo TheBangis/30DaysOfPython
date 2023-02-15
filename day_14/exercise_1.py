03583436c45292a7516e35f4c3bb0d2b7b151734
Bangis <bsani480@gmail.com>
2023-02-15 09:16:17 +0100

    day 14

--- day_14/exercise_1.py
+++
@@ -1,7 +0,0 @@
-def formated_name(f, m, l=""):
-    if l: 
-        f_n = F"{f} {m} {l}"
-    else:
-        f_n = F"{f} {m}"
-    return f_n.title()
-print(formated_name("babangida", "adam", "sani"))
