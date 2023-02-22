9212ba2ff5c47bf1dfd5b4dbba7ec9e13632bcb3
Bangis <bsani480@gmail.com>
2023-02-22 16:53:51 +0100

    day 18

--- day_18/exercise_1.py
+++ day_18/exercise_1.py
@@ -7,3 +7,9 @@ for word in words:
     dict[word] = dict.get(word,0) + 1
 sort_keysval = sorted(dict.items(),key = lambda x:x[1], reverse=True)
 print(sort_keysval)
+
+
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+pattern = r'[^\w\s]'
+clean_sentence = re.sub(pattern, "", sentence)
+print(clean_sentence)
