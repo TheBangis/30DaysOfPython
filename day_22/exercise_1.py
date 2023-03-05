9bcebf7c8df01f03e3c478b881673552de14c079
Bangis <bsani480@gmail.com>
2023-03-05 17:31:51 +0100

    day 22

---
+++ day_22/exercise_1.py
@@ -0,0 +1,34 @@
+import requests, json
+from bs4 import BeautifulSoup
+
+'''Scrape the following website and store the data as json file
+(url = 'http://www.bu.edu/president/boston-university-facts-stats/').
+
+
+url = 'http://www.bu.edu/president/boston-university-facts-stats/'
+
+response = requests.get(url)
+content = response.content
+soup = BeautifulSoup(content, 'html.parser')
+print(soup.title.get_text())
+print(soup.body.get_text())'''
+
+
+'''Extract the table in this url (https://archive.ics.uci.edu/ml/datasets.php) and change it to a json file'''
+
+
+
+import requests
+from bs4 import BeautifulSoup
+url = 'https://archive.ics.uci.edu/ml/datasets.php'
+
+response = requests.get(url)
+content = response.content 
+soup = BeautifulSoup(content, 'html.parser') 
+print(soup.title.get_text()) 
+
+tables = soup.find_all('table', {'cellpadding':'3'})
+
+table = tables[0] 
+for td in table.find('tr').find_all('td'):
+    print(json.dumps(td.text))
